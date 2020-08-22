c53a9e6ace4fc8d04962e78547770da414477351
amine <amineplastic123@gmail.com>
2020-08-22 17:48:47 +0100

    initi server
    -adding a logger : morgane
    -adding protection for http request info: helmet
    -enabling cors : cors
    -parssing json request body : express
    -running the server on port 3000
    -define user routes
    -unknown routes error handling

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res)=>{
-    res.send('hello from express')
+//register new user
+router.post('/register', async (req, res, next)=>{
+   try {
+       
+   } catch (error) {
+       
+   }
+})
+
+//return list of all users
+router.get('/users', async (req, res, next)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+//find user by his id
+router.get('/:userId', async (req, res, next)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
 })
 
 
-
-
 module.exports = router
+
